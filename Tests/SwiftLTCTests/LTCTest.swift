10aa73d6b200fe00b71ce91cda2982621fb1ff8d
ssaichixbg <git@sukicello.com>
2025-07-30 19:50:09 -0700

    Add name space

--- Tests/SwiftLTCTests/LTCTest.swift
+++ Tests/SwiftLTCTests/LTCTest.swift
@@ -6,7 +6,7 @@ struct LTCTest {
 
     @Test func TestEncoding() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        let encoder = Encoder()
+        let encoder = LTC.Encoder()
         encoder.setUserBits(10)
         let timeCode = TimeCode(timezone: "+0800", years: 25, months: 3, days: 16, hours: 1, mins: 2, secs: 3, frame: 10)
         encoder.setTimecode(timeCode)
